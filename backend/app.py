6b080d546cec883650a29bdce32841aabdb6f8d0
John N <106115048+JohnN05@users.noreply.github.com>
2024-12-27 14:57:12 -0500

    Add routes.py and clean up app.py

--- backend/app.py
+++ backend/app.py
@@ -1,44 +1,9 @@
-# backend/app.py
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
-from datetime import datetime, timedelta
-import yfinance as yf
-
 
 app = Flask(__name__)
+import routes
 CORS(app)
-
-
-@app.route("/search")
-def search():
-    ticker_symbol = request.args.get('ticker')
-    date_str = request.args.get('date')  # expected format is YYYY-MM-DD
-
-    if not ticker_symbol:
-        return jsonify({"error": "Ticker parameter is required"}), 400
-
-    if not date_str:
-        date = datetime.today()
-    else:
-        try:
-            date = datetime.strptime(date_str, '%Y-%m-%d')
-        except ValueError:
-            return jsonify({"error": "Date must be in the format YYYY-MM-DD"}), 400
-    end_date = date + timedelta(days=1)
-
-    try:
-        ticker = yf.Ticker(ticker_symbol)
-        data = ticker.history(start=date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
-
-        if data.empty:
-            return jsonify({"error": f"No data found for ticker {ticker_symbol} on date {date.strftime('%Y-%m-%d')}"}), 400
-
-        stock_info = data.iloc[0].to_dict()
-        stock_info["Date"] = date.strftime('%Y-%m-%d')
-        return jsonify({"data": stock_info})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
