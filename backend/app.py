e67b237f8dd649e54a4e46a56109a559230ffb7e
John N <106115048+JohnN05@users.noreply.github.com>
2024-12-27 15:57:55 -0500

    Create models.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,17 @@
 import routes
 from flask import Flask
 from flask_cors import CORS
+from models import db
 
 app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db.init_app(app)
+
 app.add_url_rule('/search', view_func=routes.search)
 
-CORS(app)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
