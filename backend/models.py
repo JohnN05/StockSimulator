6c64b628fdb5ee277ade1dc2356d8df3f2b19227
John N <106115048+JohnN05@users.noreply.github.com>
2024-12-28 19:49:01 -0500

    Implement basic database functionality

--- backend/models.py
+++ backend/models.py
@@ -9,14 +9,22 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.now(tz=timezone.utc))
     portfolios = db.relationship('Portfolio', backref='account')
+
+    def __repr__(self):
+        return f"User('{self.username}', {self.date_created})"
+
 
 class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.String(20), db.ForeignKey('user.username'), nullable=False)
     balance = db.Column(db.Integer, nullable=False, default=STARTING_BALANCE)
-    transactions = db.relationship('Transaction', backref='portfolio')
+    transactions = db.relationship('Transaction', backref='originator')
     last_accessed = db.Column(db.DateTime, nullable=False, default=datetime.now(tz=timezone.utc))
+
+    def __repr__(self):
+        return f"Portfolio('{self.user_id}', {self.balance}, {self.last_accessed})"
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,3 +33,6 @@ class Transaction(db.Model):
     date = db.Column(db.DateTime, nullable=False, default=datetime.now(tz=timezone.utc))
     trans_type = db.Column(db.Enum('buy','sell',name='trans_type_enum'), nullable=False)
     amount = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Transaction('{self.originator.id}', {self.ticker}, {self.date}, {self.trans_type}, {self.amount})"
