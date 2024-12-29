34e0f97e68c53713a865f4f03814d7168d8904d4
John N <106115048+JohnN05@users.noreply.github.com>
2024-12-28 19:49:50 -0500

    Add route to create users

--- backend/routes.py
+++ backend/routes.py
@@ -1,10 +1,11 @@
 from flask import request, jsonify
 from datetime import datetime, timedelta
+from models import db, User, Portfolio, Transaction
 import yfinance as yf
 
 def search():
     ticker_symbol = request.args.get('ticker')
-    date_str = request.args.get('date')  # expected format is YYYY-MM-DD
+    date_str = request.args.get('date')  # expected format: YYYY-MM-DD
 
     if not ticker_symbol:
         return jsonify({"error": "Ticker parameter is required"}), 400
@@ -28,6 +29,21 @@ def search():
         stock_info = data.iloc[0].to_dict()
         stock_info["Date"] = date.strftime('%Y-%m-%d')
         return jsonify({"data": stock_info})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+def create_user():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    if not username or not password:
+        return jsonify({'error':'Username and password are required.'}), 400
+    
+    try:
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message':f'{username} has been successfully added.'})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
